2183d827fe0780ac212e365ed6b2dcc08a7c2047
nariman079 <nariman079i@mail.ru>
2024-06-06 02:16:17 +0300

    fix settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--j#ojq8b%qtek=%)(c+=sfzyu0j=@!f-&*c#x0+6v!9zqmcz*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
